91eadbeb5cb0c8c8a898815f72e0e9f1768ee9ad
Ed Lee <edlee@fb.com>
2018-03-21 15:31:00 -0700

    creating modular components to sync dotfiles easily between home/work/work desktop computers

---
+++ rc/copy-to-facebook.sh
@@ -0,0 +1,9 @@
+# bashrc should source these modular parts
+cp bash_profile_universal ~/.bashrc_universal
+cp bash_profile_facebook ~/.bashrc_facebook
+
+# vimrc should source this universal file
+cp vimrc_universal ~/.vimrc_universal
+
+# zshrc should source this universal file
+cp zshrc_universal ~/.zshrc_universal
